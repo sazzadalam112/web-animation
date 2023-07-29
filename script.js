774d5cb80ed3c681aed8ca23b4dcb0a89a724b5c
GeekySazzad <71886049+sazzadalam112@users.noreply.github.com>
2023-07-29 23:58:16 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const ball = document.getElementById('ball');
+const play = document.getElementById('play');
+const pause = document.getElementById('pause');
+const reverse = document.getElementById('reverse');
+const speedUp = document.getElementById('speed-up');
+const speedDown = document.getElementById('speed-down');
+
