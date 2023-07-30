f3146c474ca139f41c02e7c8a92cfeb0fdeee7c0
GeekySazzad <71886049+sazzadalam112@users.noreply.github.com>
2023-07-30 23:38:33 +0530

    javascript-add

--- script.js
+++ script.js
@@ -3,5 +3,39 @@ const play = document.getElementById('play');
 const pause = document.getElementById('pause');
 const reverse = document.getElementById('reverse');
 const speedUp = document.getElementById('speed-up');
-const speedDown = document.getElementById('speed-down');
+const slowDown = document.getElementById('slow-down');
 
+const rollAnimation = [
+    {
+    transform: 'rotate(0) translate3D(-50%, -50%, 0)',
+    color: 'white', 
+},
+ {
+    color: 'blue',
+    offset: 0.1
+},
+{
+    transform: 'rotate(360deg) translate3D(-50%, -50%, 0)',
+    color: 'white', 
+},
+];
+
+const rollOptions = {
+    duration: 1000,
+    iterations:Infinity
+}
+const roll = ball.animate(rollAnimation,rollOptions);
+
+play.addEventListener('click',() =>{
+    roll.playbackRate = 1;
+    roll.play()
+});
+pause.addEventListener('click',() => roll.pause());
+reverse.addEventListener('click',() => roll.reverse());
+speedUp.addEventListener('click',
+() => (roll.playbackRate = roll.playbackRate * 2)
+);
+slowDown.addEventListener(
+    'click',
+    () => (roll.playbackRate = roll.playbackRate * 0.5)
+    );
